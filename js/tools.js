e93515b7bc35799911491a8f165b0be716cea27c
salasma <67338500+salasma@users.noreply.github.com>
2021-07-14 15:01:50 -0700

    Delete tools.js

--- js/tools.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-$('#map').usmap({
-  // The click action
-  click: function(event, data) {
-    $('#clicked-state')
-      .text('You clicked: '+data.name)
-      .parent().effect('highlight', {color: '#C7F464'}, 2000);
-  }
-});
